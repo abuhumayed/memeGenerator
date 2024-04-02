d8c6079cd7fdee007ca9e7afec96829c9540aa0e
abuhumayed <ahmedthanjim@hotmail.co.uk>
2024-04-02 15:11:05 +0100

    updates

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,27 @@
 export default function Form() {
   return (
     <main className="p-7 ">
-      <form className="grid grid-cols-2">
+      <form className="grid grid-cols-2 gap-5">
+        <label htmlFor="top-text" className=" hover:text-lpurple">
+          Top Text
+        </label>
         <input
-          type="
-              "
-          className="border"
+          id="top-text"
+          type="text"
+          className="rounded-md border-4 indent-2 "
           placeholder="Top text"
         />
-        <input type="text" className="border" />
+        <label htmlFor="bottom-text" className=" hover:text-lpurple">
+          Bottom Text
+        </label>
+        <input
+          id="bottom-text"
+          type="text"
+          className="rounded-md border-4 indent-2 "
+          placeholder="Bottom text"
+        />
 
-        <button className="bg-gradient-to-r from-dpurple to-lpurple  text-white p-2 mt-4 rounded-md col-span-2">
+        <button className="bg-gradient-to-r from-dpurple to-lpurple  text-white p-2 mt-4 rounded-md col-span-2 border cursor-pointer ">
           Get a new meme image 🖼️
         </button>
       </form>
